abe0ad18d007f085203a66cc51c8ed2570cb86a6
Josh Martin <josh.martin@visiblebody.com>
2022-02-21 02:34:34 -0500

    change db/

--- db/migrate/20201104191741_add_visitors_joined_to_rooms.rb
+++ db/migrate/20201104191741_add_visitors_joined_to_rooms.rb
@@ -1,5 +1,5 @@
 class AddVisitorsJoinedToRooms < ActiveRecord::Migration[6.0]
   def change
-    add_column :Rooms, :visitors_joined, :boolean
+    add_column :rooms, :visitors_joined, :boolean
   end
 end
